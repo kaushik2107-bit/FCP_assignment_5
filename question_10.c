a21ad772fae3a02fa0fd0cca652224c577297f0f
Kaushik Bhowmick <56549069+kaushik2107-bit@users.noreply.github.com>
2022-06-18 17:04:08 +0530

    Add files via upload

---
+++ question_10.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+	int m, n, i, j, k;
+	
+	printf("Enter the size of the 2D array: m n \n");
+	scanf("%d %d", &m, &n);
+	
+	int arr[m][n];
+	printf("Enter the values of first array: \n");
+	for (i=0; i<m; ++i) {
+		for (j=0; j<n; ++j) {
+			scanf("%d", &arr[i][j]);
+		}
+	}
+	printf("Enter the values of second array: \n");
+	int arr1[m][n];
+	for (i=0; i<m; ++i) {
+		for (j=0; j<n; ++j) {
+			scanf("%d", &arr1[i][j]);
+		}
+	}
+	
+	
+	int res[m][n];
+	for (i=0; i<m; ++i) {
+		for (j=0; j<n; ++j) {
+			res[i][j] = arr[i][j] - arr[i][j];
+		}
+	}
+	
+	
+	for (i=0; i<m; ++i) {
+		for (j=0; j<n; ++j) {
+			printf("%d ", res[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
---
+++ question_9.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int  main() {
+	int m,n,i,j;
+	scanf("%d", &m);
+	int arr0[m];
+	for (i=0; i<m; ++i) {
+	scanf("%d", &arr0[i]);
+	}
+	scanf("%d", &n);
+	int arr1[n];
+	for (i=0; i<n; ++i) {
+	scanf("%d", &arr1[i]);
+	}
+	int d = m+n;
+	int arr[d];
+	for (i=0; i<d; ++i) {
+		if (m!= 0 && n!=0) {
+			if (arr0[0] > arr1[0]) {
+				arr[i] = arr1[0];
+				n = n-1;
+				for (j=0; j<n; ++j) {
+					arr1[j] = arr1[j+1];
+				}
+			} else if (arr0[0] < arr1[0]) {
+				arr[i] = arr0[0];
+				m = m-1;
+				for (j=0; j<m; ++j) {
+					arr0[j] = arr0[j+1];
+				}
+			} else {
+				arr[i] = arr0[0];
+				arr[i+1] = arr1[0];
+				m= m-1;
+				n = n-1;
+				for (j=0; j<m; ++j) {
+					arr0[j] = arr0[j+1];
+				}
+				for (j=0; j<n; ++j) {
+					arr1[j] = arr1[j+1];
+				}
+				i+=1;
+			}
+		} else if (m==0 && n!=0) {
+			for (j=0; j<n; ++j) {
+				arr[i] = arr1[j];
+			}
+		} else if (m!=0 && n==0) {
+			for (j=0; j<m; ++j) {
+				arr[i] = arr0[j];
+			}
+		}
+	}
+	
+	for (i=0; i<d; ++i) {
+		printf("%d ", arr[i]);
+	}
+	
+	return 0;
+
+
+}
